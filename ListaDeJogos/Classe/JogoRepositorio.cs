c9f7be4d9b3c8e0f5a9019aa1a649fe92a2ec599
AlecYagi <89036771+AlecYagi@users.noreply.github.com>
2021-09-17 03:32:16 -0300

    Add files via upload

---
+++ ListaDeJogos/Classe/JogoRepositorio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeJogos
+{
+    class JogoRepositorio : IRepositorio<Jogo>
+    {
+        private List<Jogo> listaJogo = new List<Jogo>();
+        public void Atualiza(int id, Jogo objeto)
+        {
+            listaJogo[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaJogo[id].Excluir();
+        }
+
+        public void Insere(Jogo objeto)
+        {
+            listaJogo.Add(objeto);
+        }
+
+        public List<Jogo> Lista()
+        {
+            return listaJogo;
+        }
+
+        public int ProximoId()
+        {
+            return listaJogo.Count;
+        }
+
+        public Jogo RetornaPorId(int id)
+        {
+            return listaJogo[id];
+        }
+    }
+}
---
+++ ListaDeJogos/Enums/Genero.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeJogos
+{
+    public enum Genero
+    {
+        Acao = 1,
+
+        Aventura = 2,
+
+        Moba = 3,
+
+        RPG = 4,
+
+        Espionagem = 5,
+
+        Faroeste = 6,
+
+        Fantasia = 7,
+
+        Ficcao_Cientifica = 8,
+
+        Romance = 9,
+
+        Suspense = 10,
+
+        Terror = 11,
+
+        FPS = 12,
+
+        Puzzle = 13
+    }
+}
